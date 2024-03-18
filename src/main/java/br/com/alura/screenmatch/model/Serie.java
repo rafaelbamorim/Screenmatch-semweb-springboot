6edd19b0bc6eabb69425fd4ef8195d25881141d4
Rafael Amorim <rafaelamorim1189@gmail.com>
2024-03-17 23:55:58 -0300

    Relacionando serie e episodios

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -21,7 +21,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
